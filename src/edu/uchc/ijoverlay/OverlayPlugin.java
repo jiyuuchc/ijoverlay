0b53a1dec1524287a7aac6a8a2c8eae17bcc241b
Ji Yu <jyu@uchc.edu>
2019-08-16 09:18:57 -0400

    clean up

--- src/edu/uchc/ijoverlay/OverlayPlugin.java
+++ src/edu/uchc/ijoverlay/OverlayPlugin.java
@@ -35,118 +35,118 @@ public class OverlayPlugin implements PlugIn, DialogListener, ImageListener
     NonBlockingClosableDialog gd;
     
     public OverlayPlugin() {
-        this.overlayImps = new ImagePlus[3];
-        this.ipcache = new ColorProcessor[3];
-        this.xoffset = new double[3];
-        this.yoffset = new double[3];
-        this.rotation= new double[3]; 
+        overlayImps = new ImagePlus[3];
+        ipcache = new ColorProcessor[3];
+        xoffset = new double[3];
+        yoffset = new double[3];
+        rotation= new double[3]; 
     }
     
     public void run(final String cmd) {
-        this.overlayImp = null;
-        this.implist = null;
-        this.showDialog();
+        overlayImp = null;
+        implist = null;
+        showDialog();
     }
     
     void buildImpList() {
         final int nWindows = WindowManager.getImageCount();
         if (nWindows == 0) {
-            this.implist = null;
+            implist = null;
             return;
         }
-        this.implist = new ArrayList<ImagePlus>();
+        implist = new ArrayList<ImagePlus>();
         for (int i = 0; i < nWindows; ++i) {
-            this.implist.add(WindowManager.getImage(i + 1));
+            implist.add(WindowManager.getImage(i + 1));
         }
     }
     
     void getTitles() {
-        if (this.implist == null || this.implist.size() == 0) {
-            this.titles = null;
+        if (implist == null || implist.size() == 0) {
+            titles = null;
             return;
         }
-        this.titles = new String[this.implist.size() + 1];
-        for (int i = 0; i < this.implist.size(); ++i) {
-            final ImagePlus imp = this.implist.get(i);
-            this.titles[i + 1] = imp.getTitle();
+        titles = new String[implist.size() + 1];
+        for (int i = 0; i < implist.size(); ++i) {
+            final ImagePlus imp = implist.get(i);
+            titles[i + 1] = imp.getTitle();
         }
-        this.titles[0] = "None";
+        titles[0] = "None";
     }
     
     void showDialog() {
-        this.buildImpList();
-        this.getTitles();
-        if (this.titles == null) {
+        buildImpList();
+        getTitles();
+        if (titles == null) {
             IJ.noImage();
             return;
         }
-        (this.gd = new NonBlockingClosableDialog("Overlay")).addChoice("Image1:", (String[])Arrays.copyOfRange(this.titles, 1, this.titles.length), this.titles[1]);
-        this.gd.addChoice("Image2:", this.titles, this.titles[0]);
-        this.gd.addSlider("XOffset:", -100.0, 100.0, 0.0);
-        this.gd.addSlider("YOffset:", -100.0, 100.0, 0.0);
+        (gd = new NonBlockingClosableDialog("Overlay")).addChoice("Image1:", (String[])Arrays.copyOfRange(titles, 1, titles.length), titles[1]);
+        gd.addChoice("Image2:", titles, titles[0]);
+        gd.addSlider("XOffset:", -100.0, 100.0, 0.0);
+        gd.addSlider("YOffset:", -100.0, 100.0, 0.0);
         gd.addSlider("Rotation:", -90, 90, 0.0);
-        this.gd.addChoice("Image3:", this.titles, this.titles[0]);
-        this.gd.addSlider("XOffset:", -100.0, 100.0, 0.0);
-        this.gd.addSlider("YOffset:", -100.0, 100.0, 0.0);
+        gd.addChoice("Image3:", titles, titles[0]);
+        gd.addSlider("XOffset:", -100.0, 100.0, 0.0);
+        gd.addSlider("YOffset:", -100.0, 100.0, 0.0);
         gd.addSlider("Rotation:", -90, 90, 0.0);
-        this.gd.addChoice("Image4:", this.titles, this.titles[0]);
-        this.gd.addSlider("XOffset:", -100.0, 100.0, 0.0);
-        this.gd.addSlider("YOffset:", -100.0, 100.0, 0.0);
+        gd.addChoice("Image4:", titles, titles[0]);
+        gd.addSlider("XOffset:", -100.0, 100.0, 0.0);
+        gd.addSlider("YOffset:", -100.0, 100.0, 0.0);
         gd.addSlider("Rotation:", -90, 90, 0.0);
-        this.gd.addDialogListener((DialogListener)this);
-        this.overlayImp = new ImagePlus("Overlay");
+        gd.addDialogListener((DialogListener)this);
+        overlayImp = new ImagePlus("Overlay");
         ImagePlus.addImageListener((ImageListener)this);
-        this.gd.showDialog();
+        gd.showDialog();
         ImagePlus.removeImageListener((ImageListener)this);
-        if (!this.gd.wasOKed()) {
-            if (this.overlayImp != null) {
-                this.overlayImp.close();
+        if (!gd.wasOKed()) {
+            if (overlayImp != null) {
+                overlayImp.close();
             }
         }
         else {
-            this.overlayImp.duplicate().show();
-            this.overlayImp.close();
-            this.overlayImp = null;
+            overlayImp.duplicate().show();
+            overlayImp.close();
+            overlayImp = null;
         }
     }
     
     public synchronized boolean dialogItemChanged(final GenericDialog gd, final AWTEvent e) {
         final int index0 = gd.getNextChoiceIndex();
         final int[] index2 = { gd.getNextChoiceIndex(), 0, 0 };
-        this.xoffset[0] = gd.getNextNumber();
-        this.yoffset[0] = gd.getNextNumber();
+        xoffset[0] = gd.getNextNumber();
+        yoffset[0] = gd.getNextNumber();
         rotation[0] = gd.getNextNumber();
         index2[1] = gd.getNextChoiceIndex();
-        this.xoffset[1] = gd.getNextNumber();
-        this.yoffset[1] = gd.getNextNumber();
+        xoffset[1] = gd.getNextNumber();
+        yoffset[1] = gd.getNextNumber();
         rotation[2] = gd.getNextNumber();
         index2[2] = gd.getNextChoiceIndex();
-        this.xoffset[2] = gd.getNextNumber();
-        this.yoffset[2] = gd.getNextNumber();
+        xoffset[2] = gd.getNextNumber();
+        yoffset[2] = gd.getNextNumber();
         rotation[2] = gd.getNextNumber();
-        if (this.implist.get(index0) != this.firstImp) {
-            this.firstImp = this.implist.get(index0);
-            this.ip0cache = (ColorProcessor)this.firstImp.getProcessor().duplicate().convertToRGB();
+        if (implist.get(index0) != firstImp) {
+            firstImp = implist.get(index0);
+            ip0cache = (ColorProcessor)firstImp.getProcessor().duplicate().convertToRGB();
         }
         for (int k = 0; k < 3; ++k) {
             if (index2[k] != 0) {
-                if (this.implist.get(index2[k] - 1) != this.overlayImps[k]) {
-                    this.overlayImps[k] = this.implist.get(index2[k] - 1);
-                    this.ipcache[k] = (ColorProcessor)this.overlayImps[k].getProcessor().duplicate().convertToRGB();
+                if (implist.get(index2[k] - 1) != overlayImps[k]) {
+                    overlayImps[k] = implist.get(index2[k] - 1);
+                    ipcache[k] = (ColorProcessor)overlayImps[k].getProcessor().duplicate().convertToRGB();
                 }
             }
             else {
-                this.overlayImps[k] = null;
+                overlayImps[k] = null;
             }
         }
-        this.updateImage();
+        updateImage();
         return true;
     }
     
     synchronized void updateImage() {
-        final ColorProcessor ip = (ColorProcessor)this.ip0cache.duplicate();
+        final ColorProcessor ip = (ColorProcessor)ip0cache.duplicate();
         for (int k = 0; k < 3; ++k) {
-            if (this.overlayImps[k] != null) {
+            if (overlayImps[k] != null) {
             	ColorProcessor ip2;
             	if (rotation[k] == 0) {
             		ip2 = (ColorProcessor) ipcache[k];
@@ -156,93 +156,93 @@ public class OverlayPlugin implements PlugIn, DialogListener, ImageListener
 	                ip2.setInterpolationMethod(ImageProcessor.BICUBIC);
 	                ip2.rotate(rotation[k]);
             	}
-                ip.copyBits((ImageProcessor)ip2, (int)this.xoffset[k], (int)this.yoffset[k], 13);
+                ip.copyBits((ImageProcessor)ip2, (int)xoffset[k], (int)yoffset[k], 13);
             }
         }
-        this.overlayImp.setProcessor("Overlay", (ImageProcessor)ip);
-        this.overlayImp.show();
+        overlayImp.setProcessor("Overlay", (ImageProcessor)ip);
+        overlayImp.show();
     }
     
     public synchronized void imageClosed(final ImagePlus imp) {
-        if (imp == this.overlayImp) {
-            final ImageProcessor ip = this.overlayImp.getProcessor();
-            (this.overlayImp = new ImagePlus("Overlay", ip)).show();
+        if (imp == overlayImp) {
+            final ImageProcessor ip = overlayImp.getProcessor();
+            (overlayImp = new ImagePlus("Overlay", ip)).show();
             return;
         }
-        final Vector<Choice> choices = (Vector<Choice>)this.gd.getChoices();
-        if (this.implist.size() == 1) {
-            this.gd.dispose();
-            this.gd.quit();
+        final Vector<Choice> choices = (Vector<Choice>)gd.getChoices();
+        if (implist.size() == 1) {
+            gd.dispose();
+            gd.quit();
             return;
         }
-        for (int i = 0; i < this.implist.size(); ++i) {
-            if (this.implist.get(i) == imp) {
-                this.implist.remove(i);
+        for (int i = 0; i < implist.size(); ++i) {
+            if (implist.get(i) == imp) {
+                implist.remove(i);
                 break;
             }
         }
-        this.getTitles();
-        if (imp == this.firstImp) {
-            this.firstImp = this.implist.get(0);
-            this.ip0cache = (ColorProcessor)this.firstImp.getProcessor().duplicate().convertToRGB();
+        getTitles();
+        if (imp == firstImp) {
+            firstImp = implist.get(0);
+            ip0cache = (ColorProcessor)firstImp.getProcessor().duplicate().convertToRGB();
         }
         for (int k = 0; k < 3; ++k) {
-            if (this.overlayImps[k] == imp) {
-                this.overlayImps[k] = null;
+            if (overlayImps[k] == imp) {
+                overlayImps[k] = null;
             }
         }
         for (int k = 0; k < 4; ++k) {
             choices.get(k).removeAll();
         }
         for (int k = 1; k < 4; ++k) {
-            choices.get(k).add(this.titles[0]);
+            choices.get(k).add(titles[0]);
         }
         for (int k = 0; k < 4; ++k) {
-            for (int l = 1; l < this.titles.length; ++l) {
-                choices.get(k).add(this.titles[l]);
+            for (int l = 1; l < titles.length; ++l) {
+                choices.get(k).add(titles[l]);
             }
         }
-        choices.get(0).select(this.firstImp.getTitle());
+        choices.get(0).select(firstImp.getTitle());
         for (int k = 1; k < 4; ++k) {
-            if (this.overlayImps[k - 1] != null) {
-                choices.get(k).select(this.overlayImps[k - 1].getTitle());
+            if (overlayImps[k - 1] != null) {
+                choices.get(k).select(overlayImps[k - 1].getTitle());
             }
             else {
                 choices.get(k).select(0);
             }
         }
-        this.updateImage();
+        updateImage();
     }
     
     public synchronized void imageOpened(final ImagePlus imp) {
-        if (imp == this.overlayImp) {
+        if (imp == overlayImp) {
             return;
         }
-        final Vector<Choice> choices = (Vector<Choice>)this.gd.getChoices();
+        final Vector<Choice> choices = (Vector<Choice>)gd.getChoices();
         for (int i = 0; i < choices.size(); ++i) {
             final Choice c = choices.get(i);
             c.add(imp.getTitle());
         }
-        this.implist.add(imp);
+        implist.add(imp);
     }
     
     public synchronized void imageUpdated(final ImagePlus imp) {
-        if (imp == this.overlayImp) {
+        if (imp == overlayImp) {
             return;
         }
         boolean update = false;
-        if (imp == this.firstImp) {
+        if (imp == firstImp) {
             update = true;
-            this.ip0cache = (ColorProcessor)this.firstImp.getProcessor().duplicate().convertToRGB();
+            ip0cache = (ColorProcessor)firstImp.getProcessor().duplicate().convertToRGB();
         }
         for (int i = 0; i < 3; ++i) {
-            if (imp == this.overlayImps[i]) {
+            if (imp == overlayImps[i]) {
                 update = true;
-                this.ipcache[i] = (ColorProcessor)imp.getProcessor().duplicate().convertToRGB();
+                ipcache[i] = (ColorProcessor)imp.getProcessor().duplicate().convertToRGB();
             }
         }
         if (update) {
-            this.updateImage();
+            updateImage();
         }
     }
     
@@ -253,7 +253,7 @@ public class OverlayPlugin implements PlugIn, DialogListener, ImageListener
         }
         
         public synchronized void quit() {
-            this.notify();
+            notify();
         }
     }
 }
